07ce3bdf475cdb10ac4503a52226b6a7005bc81b
Hyeonjae Nam <jaemuon5582@gmail.com>
2022-01-11 16:37:58 +0900

    -fin-

--- src/main/java/moe/fotone/demo/SpringConfig.java
+++ src/main/java/moe/fotone/demo/SpringConfig.java
@@ -1,21 +1,32 @@
 package moe.fotone.demo;
 
+import moe.fotone.demo.aop.TimeTraceAop;
 import moe.fotone.demo.repository.MemberRepository;
-import moe.fotone.demo.repository.MemoryMemberRepository;
 import moe.fotone.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService () {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+
 }
--- src/main/java/moe/fotone/demo/domain/Member.java
+++ src/main/java/moe/fotone/demo/domain/Member.java
@@ -1,7 +1,13 @@
 package moe.fotone.demo.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Long getId() {
